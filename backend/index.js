a99984343c1d397dc87debec003ba22e9d4bd5fd
KUSHAL KUMAR RAYADURGAM <kushalkumarrayadurgam@KUSHALs-Mac-mini.local>
2025-06-04 19:32:59 +0530

    added route users

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,12 @@ app.get("/api", (req, res) => {
   return res.status(200).json({ message: "Hello World" });
 });
 
+
+app.get("/api/users", (req, res) => {
+  return res.status(200).json({ message: "fetched users sucessfully" });
+});
+
+
 app.listen(process.env.PORT, () => {
   console.log(`Server Running at http://localhost:${process.env.PORT}`);
 });
