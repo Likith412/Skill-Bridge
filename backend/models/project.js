b2bc771af1e8cd7354e82f43020b6ec0e31399dd
Likith412 <metikalalikithkumar.gtl@gmail.com>
2025-06-05 23:59:35 +0530

    updated project schema

--- backend/models/project.js
+++ backend/models/project.js
@@ -1,24 +1,25 @@
 const mongoose = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  budget: { type: Number, required: true },
-  deadline: { type: Date, required: true },
-  requiredSkills: [String],
-  status: {
-    type: String,
-    enum: ["open", "in-progress", "completed", "cancelled"],
-    default: "open",
+const projectSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    budget: { type: Number, required: true },
+    deadline: { type: Date, required: true },
+    requiredSkills: [String],
+    status: {
+      type: String,
+      enum: ["open", "in-progress", "completed", "cancelled"],
+      default: "open",
+    },
+    clientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  clientId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  createdAt: { type: Date, default: Date.now },
-  updateAt: { type: Date, default: Date.now },
-});
+  { timestamps: true }
+);
 
 const Project = mongoose.model("Project", projectSchema);
 module.exports = Project;
