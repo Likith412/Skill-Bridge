6f877fbe3d93b4a99ec0c7cddb2b9624873ff071
Likith412 <metikalalikithkumar.gtl@gmail.com>
2025-06-06 19:18:34 +0530

    Revert "project create,update,delete controller"
    This reverts commit afe8f7b82a23e494734215b64e4a21eab99ecbce.

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 
 const { connectToMongoDB } = require("./connection");
 const userRouter = require("./routes/user");
-const projectRouter = require("./routes/project");
 
 const app = express();
 
@@ -17,10 +16,6 @@ app.use(express.json());
 
 // Router Registrations
 app.use("/api/users", userRouter);
-
-
-// Project Routes
-app.use("/api/projects", projectRouter);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
