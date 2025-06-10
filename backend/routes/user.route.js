2dee9d293029fc110d3dff049acb2f0ecf09355c
mithilesh <mithileshthallam@gmail.com>
2025-06-10 23:37:42 +0530

    error fixed in route file

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { upload } = require("../middlewares/multer.middleware");
+const { profileUpload } = require("../middlewares/multer.middleware");
 
 const { handleRegisterUser, handleLoginUser } = require("../controllers/user.controller");
 
 const router = express.Router();
 
-router.post("/register", upload.single("userImage"), handleRegisterUser);
+router.post("/register", profileUpload.single("userImage"), handleRegisterUser);
 router.post("/login", handleLoginUser);
 
 module.exports = router;
